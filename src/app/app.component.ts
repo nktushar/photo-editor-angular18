52e6cd8abf88965d22564982c2f94fc5f7330e10
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-09-11 00:08:08 +0600

    file upload done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'image-editor-angular-markerjs-mediapipe';
+  uploadedImage: File | null = null;
+
+  // Trigger the hidden file input
+  onUploadClick(): void {
+    const fileInput = document.querySelector(
+      'input[type="file"]'
+    ) as HTMLElement;
+    fileInput.click();
+  }
+
+  // Save the uploaded file in the component variable
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.uploadedImage = input.files[0];
+      console.log('Uploaded Image:', this.uploadedImage);
+    }
+  }
 }
