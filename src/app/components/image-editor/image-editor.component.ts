c6557739dd85e5e9cafdcd5f8e44603a8ded1c5c
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-09-19 00:00:35 +0600

    kjdfkljd

--- src/app/components/image-editor/image-editor.component.ts
+++ src/app/components/image-editor/image-editor.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Input,
+  input,
+  model,
+  effect,
+  inject,
+  OnInit,
+  Output,
+  Component,
+  EventEmitter,
+} from '@angular/core';
+
+import * as cropro from 'cropro';
+import * as markerjs2 from 'markerjs2';
+
+// import { AIService } from "src/app/services/ai-service/ai-service.service";
 
 @Component({
   selector: 'app-image-editor',
   standalone: true,
   imports: [],
   templateUrl: './image-editor.component.html',
-  styleUrl: './image-editor.component.scss'
+  styleUrl: './image-editor.component.scss',
 })
-export class ImageEditorComponent {
-
+export class ImageEditorComponent implements OnInit {
+  ngOnInit() {}
+  constructor() {}
 }
